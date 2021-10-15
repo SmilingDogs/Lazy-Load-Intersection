8999180ebdce7ba1c1ba420ed89da49042775a66
Smiling Dog <a.kudr74@gmail.com>
2021-10-15 14:51:43 +0300

    add fading from left(odd) and right(even)

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const targets = document.querySelectorAll("img");
 const lazyLoad = (target) => {
   const io = new IntersectionObserver((entries, observer) => {
     console.log(entries);
+    let ind=true;
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         const img = entry.target;
         const src = img.getAttribute("data-lazy");
-
+        /* if (ind){
+          img.style.transformX="50%"
+          console.log(123);
+          ind=false;
+        }
+        else ind=true; */
+        console.log(456);
         img.src = src;
         img.classList.add("fade");
-
+        //img.style.transform="translateX(0%)"
         observer.disconnect();
       }
     });
